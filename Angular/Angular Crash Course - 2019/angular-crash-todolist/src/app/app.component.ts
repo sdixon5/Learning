4db8d346e036cb01391b720658fb0994dc905a2e
sdixon5 <shawn12dixon@gmail.com>
2019-09-04 21:06:46 -0600

    half way

---
+++ Angular/Angular Crash Course - 2019/angular-crash-todolist/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  //the following :string is typescript
+  // name:string = 'Ivonne';
+
+  // constructor(){
+  //   console.log(123);
+  //   //this.name = 'Jasmine'
+  //   this.changeName('Jasmine');
+  // }
+
+  // changeName(name:string):void{
+  //   this.name = name;
+  // }
+}
---
+++ Angular/Angular Crash Course - 2019/angular-crash-todolist/src/app/models/Todo.ts
@@ -0,0 +1,7 @@
+export class Todo {
+    id:number;
+    //? makes it optional
+    //id?:number:
+    title:string
+    completed:boolean;
+}
