5392a5dd5b8e0edb8fd79e865c5ceaf180df575f
sdixon5 <shawn12dixon@gmail.com>
2019-12-08 20:58:39 -0700

    react hooks and forms

--- React Net Core/Reactivities/client-app/src/app/features/activities/dashboard/ActivityDashboard.tsx
+++ React Net Core/Reactivities/client-app/src/app/features/activities/dashboard/ActivityDashboard.tsx
@@ -6,18 +6,38 @@ import ActivityDetails from "../details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
 
 interface IProps {
-    activities: IActivity[]
+  activities: IActivity[];
+  selectActivity: (id: string) => void;
+  selectedActivity: IActivity | null | undefined;
+  editMode: boolean;
+  setEditMode: (editMode: boolean) => void;
+  setSelectedActivity: (activity: IActivity | null) => void;
 }
 
-const ActivityDashboard: React.FC<IProps> = ({activities}) => {
+const ActivityDashboard: React.FC<IProps> = ({
+  activities,
+  selectActivity,
+  selectedActivity,
+  editMode,
+  setEditMode,
+  setSelectedActivity
+}) => {
   return (
     <Grid>
       <Grid.Column width={10}>
-          <ActivityList activities={activities}/>
+        <ActivityList activities={activities} selectActivity={selectActivity} />
       </Grid.Column>
       <Grid.Column width={6}>
-          <ActivityDetails />
-          <ActivityForm />
+        {selectedActivity && !editMode && (
+          <ActivityDetails
+            activity={selectedActivity}
+            setEditMode={setEditMode}
+            setSelectedActivity={setSelectedActivity}
+          />
+        )}
+        {editMode && (
+          <ActivityForm setEditMode={setEditMode} activity={selectedActivity!} />
+        )}
       </Grid.Column>
     </Grid>
   );
