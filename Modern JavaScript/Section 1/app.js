71e14be791b0471a9d8d1ec621baa405f0300890
sdixon5 <shawn12dixon@gmail.com>
2019-12-10 20:27:06 -0700

    console

--- Modern JavaScript/Section 1/app.js
+++ Modern JavaScript/Section 1/app.js
@@ -1,1 +1,22 @@
-alert('Hello World');
+// alert('Hello World');
+
+//Log to console
+console.log('Hello world');
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+
+console.error('This is some error');
+console.clear();
+console.warn('Watch out');
+console.time('Hello');
+    console.log('Hello world');
+    console.log('Hello world');
+    console.log('Hello world');
+    console.log('Hello world');
+    console.log('Hello world');
+console.timeEnd('Hello');
