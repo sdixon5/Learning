dee55fed1e0ac16f40df185abb461dfa389eb9ad
SHAWN DIXON <SDIXON3@greeleyschools.org>
2019-12-12 15:59:25 -0700

    ts no constructor?

--- React Developer 2020/Section 3 React Basics/Monsters-Rolodex TSX/monsters-rolodex/src/App.tsx
+++ React Developer 2020/Section 3 React Basics/Monsters-Rolodex TSX/monsters-rolodex/src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const App: React.FC = () => {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  // constructor(props: any){
+  //   super(props);
+
+  //   this.state = {
+  //     string: "Hello Shawn Dixon"
+  //   }
+
+  state = {
+    string: "Hello Shawn Dixon"
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.string}</p>
+          <button onClick={() => this.setState({ string: "Hello IronMan" })}>
+            Change Text
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
