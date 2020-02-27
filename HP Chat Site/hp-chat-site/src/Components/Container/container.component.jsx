6ff4d27e0136729b042157a418380f32be4c0942
sdixon5 <shawn12dixon@gmail.com>
2020-02-26 18:24:18 -0700

    attempting to recreate in react

---
+++ HP Chat Site/hp-chat-site/src/Components/Container/container.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './container.component.css';
+import { Container } from 'semantic-ui-react';
+
+// export default function container() {
+//     return (
+//         <div>
+            
+//         </div>
+//     )
+// }
+
+export const Container = () => {
+    return (
+        <Container>
+
+        </Container>
+    );
+};
---
+++ HP Chat Site/hp-chat-site/src/Components/Grid/grid.component.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import './grid.component.css';
+import { Grid } from 'semantic-ui-react';
+
