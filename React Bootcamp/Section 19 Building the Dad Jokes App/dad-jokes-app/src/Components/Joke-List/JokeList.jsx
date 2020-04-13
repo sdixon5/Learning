540ffddb14397d8d4980b35e7bfae2e53a662122
sdixon5 <shawn12dixon@gmail.com>
2020-04-13 17:30:43 -0600

    more work on section 19

--- React Bootcamp/Section 19 Building the Dad Jokes App/dad-jokes-app/src/Components/Joke-List/JokeList.jsx
+++ React Bootcamp/Section 19 Building the Dad Jokes App/dad-jokes-app/src/Components/Joke-List/JokeList.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
+import Joke from "../Joke/Joke";
+import { v4 as uuid } from "uuid";
 import "./JokeList.css";
 
 export class JokeList extends Component {
@@ -20,12 +22,20 @@ export class JokeList extends Component {
             let res = await axios.get("https://icanhazdadjoke.com/", {
                 headers: { Accept: "application/json" },
             });
-            jokes.push(res.data.joke);
+            jokes.push({ id: uuid(), text: res.data.joke, votes: 0 });
         }
 
         this.setState({
             jokes: jokes,
         });
+    }
+
+    handleVote(id, delta) {
+        this.setState((st) => ({
+            jokes: st.jokes.map((j) =>
+                j.id === id ? { ...j, votes: j.votes + delta } : j
+            ),
+        }));
     }
 
     render() {
@@ -40,7 +50,13 @@ export class JokeList extends Component {
                 </div>
                 <div className="JokeList-jokes">
                     {this.state.jokes.map((j) => (
-                        <div>{j}</div>
+                        <Joke
+                            key={j.id}
+                            votes={j.votes}
+                            text={j.text}
+                            upvote={() => this.handleVote(j.id, 1)}
+                            downvote={() => this.handleVote(j.id, -1)}
+                        />
                     ))}
                 </div>
             </div>
