c38dbfa759051cc080502f1ed9b071a27becf841
sdixon5 <shawn12dixon@gmail.com>
2020-05-04 15:02:58 -0600

    end of lesson 174

---
+++ React Bootcamp/Massive Color Project/color-project/src/Palette.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export class Palette extends Component {
+    render() {
+        return (
+            <div className='Palette'>
+                {/* Nav Goes Here */}
+                <div className='Palette-colors'>
+                    {/* bunch of color boxes */}
+                </div>
+                {/* footer eventually */}
+            </div>
+        )
+    }
+}
+
+export default Palette
