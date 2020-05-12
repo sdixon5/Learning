a03b71a7fc5820cd78fcabde40dd006d43aec363
sdixon5 <shawn12dixon@gmail.com>
2020-05-12 13:58:53 -0600

    end of section 6

--- React Net Core/Reactivities/client-app/src/app/features/activities/form/ActivityForm.tsx
+++ React Net Core/Reactivities/client-app/src/app/features/activities/form/ActivityForm.tsx
@@ -7,14 +7,16 @@ interface IProps {
   setEditMode: (editMode: boolean) => void;
   activity: IActivity;
   createActivity: (activity: IActivity) => void;
-    editActivity: (activity: IActivity) => void;
+  editActivity: (activity: IActivity) => void;
+  submitting: boolean;
 }
 
 const ActivityForm: React.FC<IProps> = ({
   setEditMode,
   activity: initialFormState,
   createActivity,
-  editActivity
+  editActivity,
+  submitting
 }) => {
   const initializeForm = () => {
     if (initialFormState) {
@@ -92,7 +94,7 @@ const ActivityForm: React.FC<IProps> = ({
           placeholder="Venue"
           value={activity.venue}
         />
-        <Button floated="right" positive type="submit" content="Submit" />
+        <Button loading={submitting} floated="right" positive type="submit" content="Submit" />
         <Button
           onClick={() => setEditMode(false)}
           floated="right"
