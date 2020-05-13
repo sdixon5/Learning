c14867db105447d255b30567a0e9cc222168c4d0
sdixon5 <shawn12dixon@gmail.com>
2020-05-13 11:57:59 -0600

    Slider plus colorHelpers

--- React Bootcamp/Massive Color Project/color-project/src/App.js
+++ React Bootcamp/Massive Color Project/color-project/src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import Palette from './Palette';
-import seedColors from './seedColors';
+import React from "react";
+import Palette from "./Palette";
+import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
 
 function App() {
+  console.log(generatePalette(seedColors[4]));
   return (
     <div>
-      <Palette {...seedColors[4]} />
+      <Palette palette={generatePalette(seedColors[4])} />
     </div>
   );
 }
