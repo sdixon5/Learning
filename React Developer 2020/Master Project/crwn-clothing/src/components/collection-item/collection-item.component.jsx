be781462f736673c184d64749268c9e4df21f715
sdixon5 <shawn12dixon@gmail.com>
2020-05-13 15:22:44 -0600

    cart, part way

--- React Developer 2020/Master Project/crwn-clothing/src/components/collection-item/collection-item.component.jsx
+++ React Developer 2020/Master Project/crwn-clothing/src/components/collection-item/collection-item.component.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 
 import "./collection-item.styles.scss";
+import CustomButton from "../custom-button/custom-button.component";
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
-    <div className="collection-item">
-        <div
-            className="image"
-            style={{ backgroundImage: `url(${imageUrl})` }}
-        ></div>
-        <div className="collection-footer">
-            <span className="name">{name}</span>
-            <span className="price">{price}</span>
-        </div>
+  <div className="collection-item">
+    <div
+      className="image"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    ></div>
+    <div className="collection-footer">
+      <span className="name">{name}</span>
+      <span className="price">{price}</span>
     </div>
+    <CustomButton inverted> Add To Cart </CustomButton>
+  </div>
 );
 
 export default CollectionItem;
--- React Developer 2020/Master Project/crwn-clothing/src/components/header/header.component.jsx
+++ React Developer 2020/Master Project/crwn-clothing/src/components/header/header.component.jsx
@@ -1,38 +1,46 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { connect } from 'react-redux';
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-import {ReactComponent as Logo } from '../../assets/crown.svg'
+import { ReactComponent as Logo } from "../../assets/crown.svg";
 
-import './header.styles.scss'
-import { auth } from '../../firebase/firebase.utils';
+import "./header.styles.scss";
+import { auth } from "../../firebase/firebase.utils";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({ currentUser }) => {
-    return (
-        <div className='header'>
-            <Link className='logo-container' to='/'>
-                <Logo className='logo'/>
-            </Link>
-            <div className='options'>
-                <Link className='option' to='/shop'>
-                    SHOP
-                </Link>
-                <Link className='option' to='/shop'>
-                    CONTACT
-                </Link>
-                {
-                    currentUser ?
-                    <div className='option' onClick={() => auth.signOut()} >SIGN OUT</div>
-                    :
-                    <Link className='option' to='/signin'>SIGN IN</Link>
-                }
-            </div>
-        </div>
-    )
-}
+const Header = ({ currentUser, hidden }) => {
+  return (
+    <div className="header">
+      <Link className="logo-container" to="/">
+        <Logo className="logo" />
+      </Link>
+      <div className="options">
+        <Link className="option" to="/shop">
+          SHOP
+        </Link>
+        <Link className="option" to="/shop">
+          CONTACT
+        </Link>
+        {currentUser ? (
+          <div className="option" onClick={() => auth.signOut()}>
+            SIGN OUT
+          </div>
+        ) : (
+          <Link className="option" to="/signin">
+            SIGN IN
+          </Link>
+        )}
+        <CartIcon />
+      </div>
+      {hidden ? null : <CartDropdown />}
+    </div>
+  );
+};
 
-const mapStateToProps = (state) => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
