e45552e58018b28c7e48be508b8aad3a43159a6d
sdixon5 <shawn12dixon@gmail.com>
2020-05-13 16:20:55 -0600

    cart upgrades

--- React Developer 2020/Master Project/crwn-clothing/src/components/preview-collection/collection-preview.component.jsx
+++ React Developer 2020/Master Project/crwn-clothing/src/components/preview-collection/collection-preview.component.jsx
@@ -4,18 +4,18 @@ import "./collection-preview.styles.scss";
 import CollectionItem from "../collection-item/collection-item.component";
 
 const CollectionPreview = ({ title, items }) => {
-    return (
-        <div className="collection-preview">
-            <h1 className="title">{title.toUpperCase()}</h1>
-            <div className="preview">
-                {items
-                    .filter((item, idx) => idx < 4)
-                    .map(({ id, ...otherItemProps}) => (
-                        <CollectionItem key={id} {...otherItemProps} />
-                    ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="collection-preview">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <div className="preview">
+        {items
+          .filter((item, idx) => idx < 4)
+          .map((item) => (
+            <CollectionItem key={item.id} item={item} />
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default CollectionPreview;
