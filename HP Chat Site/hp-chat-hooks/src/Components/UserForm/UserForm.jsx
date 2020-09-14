fa5d1219667974cb55e8fd650ad05ea5c3a78fc4
sdixon5 <shawn12dixon@gmail.com>
2020-09-13 20:05:55 -0600

    changed user form to toggle

--- HP Chat Site/hp-chat-hooks/src/Components/UserForm/UserForm.jsx
+++ HP Chat Site/hp-chat-hooks/src/Components/UserForm/UserForm.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Form } from "semantic-ui-react";
+import React, { useState } from "react";
+import { Accordion, AccordionPanel, Form, Icon } from "semantic-ui-react";
 
 export default function UserForm({
   name,
@@ -9,20 +9,38 @@ export default function UserForm({
   setPhone,
   setEmail,
 }) {
+  const [activeIndex, setActiveIndex] = useState(-1);
+
+  const handleClick = () => {
+    setActiveIndex(activeIndex === 0 ? -1 : 0);
+  };
+
   return (
-    <Form>
-      <Form.Field>
-        <label>Name</label>
-        <input placeholder="Name" value={name} onChange={setName} />
-      </Form.Field>
-      <Form.Field>
-        <label>Phone</label>
-        <input placeholder="Phone" value={phone} onChange={setPhone} />
-      </Form.Field>
-      <Form.Field>
-        <label>Email</label>
-        <input placeholder="Email" value={email} onChange={setEmail} />
-      </Form.Field>
-    </Form>
+    <Accordion styled>
+      <Accordion.Title
+        active={activeIndex === 0}
+        index={0}
+        onClick={handleClick}
+      >
+        <Icon name="dropdown" />
+        User Information
+      </Accordion.Title>
+      <Accordion.Content active={activeIndex === 0}>
+        <Form>
+          <Form.Field>
+            <label>Name</label>
+            <input placeholder="Name" value={name} onChange={setName} />
+          </Form.Field>
+          <Form.Field>
+            <label>Phone</label>
+            <input placeholder="Phone" value={phone} onChange={setPhone} />
+          </Form.Field>
+          <Form.Field>
+            <label>Email</label>
+            <input placeholder="Email" value={email} onChange={setEmail} />
+          </Form.Field>
+        </Form>
+      </Accordion.Content>
+    </Accordion>
   );
 }
