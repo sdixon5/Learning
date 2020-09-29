73a726d177f2a2a1092206c061954505e86dbef1
sdixon5 <shawn12dixon@gmail.com>
2020-09-28 19:09:49 -0600

    added firebase config cdn settings

---
+++ HP Chat Site/hp-chat-hooks/src/Firebase/firebase.js
@@ -0,0 +1,24 @@
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="https://www.gstatic.com/firebasejs/7.21.1/firebase-app.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries -->
+// <script src="https://www.gstatic.com/firebasejs/7.21.1/firebase-analytics.js"></script>
+
+// <script>
+//   // Your web app's Firebase configuration
+//   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//   var firebaseConfig = {
+//     apiKey: "AIzaSyAnnwhz0i0wExPBzDiRlD6QcG9B82t_URM",
+//     authDomain: "hpchat-c3d47.firebaseapp.com",
+//     databaseURL: "https://hpchat-c3d47.firebaseio.com",
+//     projectId: "hpchat-c3d47",
+//     storageBucket: "hpchat-c3d47.appspot.com",
+//     messagingSenderId: "914780045579",
+//     appId: "1:914780045579:web:bf48b4ee0b4bc5724ae20e",
+//     measurementId: "G-VNBS7W0EJC"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+//   firebase.analytics();
+// </script>
