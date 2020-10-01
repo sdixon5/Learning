f8d8acae2ea647b38fad15dacc449fb2ef54136e
SHAWN DIXON <SDIXON3@greeleyschools.org>
2020-10-01 11:27:08 -0600

    I was able to fix the copy to clipboard issue. I was using a useEffect hook and it was not needed. Over development of the logic was the real issue.

--- HP Chat Site/hp-chat-hooks/src/Components/SectionItem/SectionItem.jsx
+++ HP Chat Site/hp-chat-hooks/src/Components/SectionItem/SectionItem.jsx
@@ -2,16 +2,25 @@ import React, { useState, useEffect } from "react";
 import { Item, Button } from "semantic-ui-react";
 
 export default function SectionItem({ response }) {
-  const [value, setValue] = useState("");
+  //const [value, setValue] = useState("");
 
-  const handleChange = (e) => {
-    setValue(response);
+  // const handleChange = (e, response) => {
+  //   console.log(response);
+  //   setValue(response);
+  // };
+
+  const handleClick = () => {
+    let text = response.replace(/<[^>]*>?/gm, "\n");
+    //console.log(text);
+    navigator.clipboard.writeText(text);
   };
 
-  useEffect(() => {
-    let text = value.replace(/<[^>]*>?/gm, "\n");
-    navigator.clipboard.writeText(text);
-  }, [value]);
+  // useEffect(() => {
+  //   //let text = value.replace(/<[^>]*>?/gm, "\n");
+  //   let text = response.replace(/<[^>]*>?/gm, "\n");
+  //   console.log(text);
+  //   navigator.clipboard.writeText(text);
+  // }, [value]);
 
   return (
     <Item>
@@ -19,7 +28,12 @@ export default function SectionItem({ response }) {
         <p dangerouslySetInnerHTML={{ __html: response }}></p>
       </Item.Content>
       <Item.Extra>
-        <Button size="tiny" color="blue" floated="right" onClick={handleChange}>
+        <Button
+          size="tiny"
+          color="blue"
+          floated="right"
+          onClick={() => handleClick()}
+        >
           Copy To Clipboard
         </Button>
       </Item.Extra>
