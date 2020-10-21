3cdb384b75528422872c2f6bfd4f97d1934344e7
sdixon5 <shawn12dixon@gmail.com>
2020-10-20 22:11:25 -0600

    starting firebase configuration

--- HP Chat Site/hp-chat-hooks/src/Firebase/firebase.js
+++ HP Chat Site/hp-chat-hooks/src/Firebase/firebase.js
@@ -1,24 +1,16 @@
-// <!-- The core Firebase JS SDK is always required and must be listed first -->
-// <script src="https://www.gstatic.com/firebasejs/7.21.1/firebase-app.js"></script>
+import firebase from "firebase/app";
+import "firebase/auth";
 
-// <!-- TODO: Add SDKs for Firebase products that you want to use
-//      https://firebase.google.com/docs/web/setup#available-libraries -->
-// <script src="https://www.gstatic.com/firebasejs/7.21.1/firebase-analytics.js"></script>
+const app = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+});
 
-// <script>
-//   // Your web app's Firebase configuration
-//   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-//   var firebaseConfig = {
-//     apiKey: "AIzaSyAnnwhz0i0wExPBzDiRlD6QcG9B82t_URM",
-//     authDomain: "hpchat-c3d47.firebaseapp.com",
-//     databaseURL: "https://hpchat-c3d47.firebaseio.com",
-//     projectId: "hpchat-c3d47",
-//     storageBucket: "hpchat-c3d47.appspot.com",
-//     messagingSenderId: "914780045579",
-//     appId: "1:914780045579:web:bf48b4ee0b4bc5724ae20e",
-//     measurementId: "G-VNBS7W0EJC"
-//   };
-//   // Initialize Firebase
-//   firebase.initializeApp(firebaseConfig);
-//   firebase.analytics();
-// </script>
+export const auth = app.auth();
+export default app;
