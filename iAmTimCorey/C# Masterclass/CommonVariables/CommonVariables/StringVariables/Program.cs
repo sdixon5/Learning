84303e36f6a84d10c5b1b4debc992309b66f9f5d
sdixon5 <shawn12dixon@gmail.com>
2023-03-28 15:39:16 -0600

    Start of common variables section

---
+++ iAmTimCorey/C# Masterclass/CommonVariables/CommonVariables/StringVariables/Program.cs
@@ -0,0 +1,31 @@
+﻿//camelCase
+//PascalCase
+//strHungarianNotation was pushed by microsoft at one point in time. It tells you the type before the variable name.
+
+string firstName = string.Empty;
+string lastName = string.Empty;
+string filePath = string.Empty;
+
+firstName = "Tim";
+lastName = "Corey";
+
+// \ is an escape character, you need two \\ inside of a file path
+filePath = "C:\\Temp\\Demo";
+
+//the @ makes it so that everything in the string is a literal string aka @ makes the string a string literal
+filePath = @"C:\Temp\Demo";
+
+//you can mix and match, the order of $@ no longer matters, though it used to
+string testString = $@"The file for {firstName} is at C:\SampleFiles";
+
+//firstName = "123";
+
+//" " is a string literal in the cw below
+//Console.WriteLine(firstName + " " + lastName);
+
+//String interpolation
+Console.WriteLine($"{firstName} {lastName}");
+
+Console.WriteLine(filePath);
+
+//strings are expensive on the 10,000+ scale. At smaller scales they are fine.
---
+++ iAmTimCorey/C# Masterclass/MySecondProject/MySecondProject/GreetingApp/Program.cs
@@ -0,0 +1,14 @@
+﻿//Welcome to user app
+Console.WriteLine("Welcome to the Greeting Application!");
+Console.WriteLine();
+
+//Ask for first name
+Console.Write("What is your first name: ");
+string? firstName = Console.ReadLine();
+
+//greet user by name
+Console.WriteLine();
+Console.WriteLine($"Hello, {firstName}!");
+
+Console.WriteLine("Thanks for using the application!");
+Console.ReadLine();
