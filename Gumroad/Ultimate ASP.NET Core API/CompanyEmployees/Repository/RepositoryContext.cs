76e13d4b4cb50c95b0820c790e21b27fda74f2b4
sdixon5 <shawn12dixon@gmail.com>
2023-08-11 15:33:03 -0600

    End of 3.3

---
+++ Gumroad/Ultimate ASP.NET Core API/CompanyEmployees/Repository/RepositoryContext.cs
@@ -0,0 +1,18 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Company>? Companies { get; set; }
+        public DbSet<Employee>? Employees { get; set; }
+    }
+}
