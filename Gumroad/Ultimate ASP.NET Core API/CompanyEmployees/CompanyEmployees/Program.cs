9f50185f9e034d013a333a729c2c3fd80b93f48d
sdixon5 <shawn12dixon@gmail.com>
2023-08-17 17:19:08 -0600

    end of 3.9, end of chapter 3

--- Gumroad/Ultimate ASP.NET Core API/CompanyEmployees/CompanyEmployees/Program.cs
+++ Gumroad/Ultimate ASP.NET Core API/CompanyEmployees/CompanyEmployees/Program.cs
@@ -14,6 +14,8 @@ builder.Services.ConfigureLoggerService();
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
 
+builder.Services.ConfigureSqlContext(builder.Configuration);
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
