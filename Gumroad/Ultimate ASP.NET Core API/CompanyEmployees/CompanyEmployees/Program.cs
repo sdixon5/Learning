d962cb8ba62edd2c0a3275568a052cdb04fb841d
sdixon5 <shawn12dixon@gmail.com>
2023-08-17 17:14:15 -0600

    End of 3.8

--- Gumroad/Ultimate ASP.NET Core API/CompanyEmployees/CompanyEmployees/Program.cs
+++ Gumroad/Ultimate ASP.NET Core API/CompanyEmployees/CompanyEmployees/Program.cs
@@ -12,6 +12,7 @@ builder.Services.ConfigureIISIntegrations();
 builder.Services.ConfigureLoggerService();
 
 builder.Services.ConfigureRepositoryManager();
+builder.Services.ConfigureServiceManager();
 
 builder.Services.AddControllers();
 
