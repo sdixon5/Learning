339502421f5663a653ba75f5f58a51aaef79dcad
sdixon5 <shawn12dixon@gmail.com>
2024-02-02 16:05:37 -0700

    leetcode #3

---
+++ Leetcode/3/longestSubstringNoRepeats/longestSubstringNoRepeats/Program.cs
@@ -0,0 +1,9 @@
+﻿using longestSubstringNoRepeats;
+
+Solution solution = new Solution();
+
+//Console.WriteLine(solution.LengthOfLongestSubstring("abcabcbb")); //abc
+//Console.WriteLine(solution.LengthOfLongestSubstring("bbbbb")); //b
+//Console.WriteLine(solution.LengthOfLongestSubstring("pwwkew")); //wke
+//Console.WriteLine(solution.LengthOfLongestSubstring(" ")); //' ' length of 1
+Console.WriteLine(solution.LengthOfLongestSubstring("au")); // 2
